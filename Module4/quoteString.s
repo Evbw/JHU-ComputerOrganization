214051d0167ed6cc66117da7ccd49bb914fa47a1
Bowline <sp25bcd8b7@rpi-buster-armhf>
2025-02-12 18:36:35 +0000

    Added quotes around input string for output

--- Module4/quoteString.s
+++ Module4/quoteString.s
@@ -24,6 +24,6 @@ main:
 
 .data
 	prompt: .asciz "Enter a sentence so I can put quotes around it\n"
-	input: .asciz "%99[ ]"
-	output: .asciz "You typed the following: %s\n"
+	input: .asciz "%s"
+	output: .asciz "You typed the following: \"%s\"\n"
 	sentence: .space 99
