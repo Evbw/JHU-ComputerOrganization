19580022df6464d9811f05cdf67d18dbc3664d76
Bowline <sp25bcd8b7@rpi-buster-armhf>
2025-02-12 16:42:23 +0000

    Added program content

--- Module4/quoteString.s
+++ Module4/quoteString.s
@@ -7,11 +7,21 @@ main:
 	SUB sp, sp, #4
 	STR lr, [sp, #0]
 
+	LDR r0, =prompt
+	BL printf
 
+	LDR r0, =input
+	BL scanf
+
+	LDR r0, =output
+	LDR r0, [r0, #0]
+	BL printf
 
 	LDR lr, [sp, #0]
 	ADD sp, sp, #4
 	MOV pc, lr
 
 .data
-	
+	prompt: .asciz "Enter a string so I can put quotes around it\n"
+	input: .asciz "%s"
+	output: .asciz "%s"	
