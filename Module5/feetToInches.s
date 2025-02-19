6c8c66e2c926c8e60014d8f4f040823b45f03892
Bowline <sp25bcd8b7@rpi-buster-armhf>
2025-02-19 22:04:06 +0000

    Finish logic for accepting multiple inputs

--- Module5/feetToInches.s
+++ Module5/feetToInches.s
@@ -8,16 +8,24 @@ main:
 	SUB sp, sp, #4
 	STR lr, [sp, #0]
 
-	LDR r0, =prompt
+	LDR r0, =prompt1
 	BL printf
 
-	LDR r0, =input1
-	LDR r2, =num1
+	LDR r0, =input
+	LDR r1, =num1
 	BL scanf
 
-	LDR r1, =input2
-	LDR r3, =num2
+	LDR r0, =prompt2
+	BL printf
+
+	LDR r0, =input
+	LDR r1, =num2
 	BL scanf
+
+	LDR r1, =num1
+	LDR r1, [r1]
+	LDR r2, =num2
+	LDR r2, [r2]
 
 	LDR r0, =output
 	BL printf
@@ -27,9 +35,9 @@ main:
 	MOV pc, lr
 
 .data
-	prompt: .asciz "Please enter a number of feet and inches to convert them to feet (and inches):\n"
-	input1: .asciz "%d"
-	input2: .asciz "%d"
+	prompt1: .asciz "Please enter a number of feet to convert them to feet (and leftover inches):\n"
+	prompt2: .asciz "If you have any additional inches, add them here:\n"
+	input: .asciz "%d"
 	num1: .word 0
 	num2: .word 0
 	output: .asciz "The first number was %d and the second number was %d\n"
