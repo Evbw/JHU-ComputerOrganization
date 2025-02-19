dde34ec52ab3d80c6e1a578bd85fb5ab0e04bfd1
Bowline <sp25bcd8b7@rpi-buster-armhf>
2025-02-19 20:07:03 +0000

    Clean up logic for correct(ish) output

--- Module5/fahrenheitToCelsius.s
+++ Module5/fahrenheitToCelsius.s
@@ -17,14 +17,15 @@ main:
 
 	LDR r0, =degrees
 	LDR r0, [r0]
-	SUB r1, r0, #-32
-	MOV r2, #5
-	MUL r1, r1, r2
-	MOV r2, #9
-	BL __aeabi_idiv
-	MOV r3, r1
+	MOV r5, r0		//Store original value in r5
+	SUB r0, r0, #32
+	MOV r1, #5
+	MUL r0, r0, r1
+	MOV r1, #9
+	BL __aeabi_idiv		//Final conversion
 
-	MOV r2, r1
+	MOV r2, r0		//Store converted value in R2
+	MOV r1, r5		//Restore original value to r1
 	LDR r0, =output
 	BL printf
 
@@ -36,4 +37,4 @@ main:
 	prompt: .asciz "Enter a number in degrees Fahrenheit to convert to Celsius:\n"
 	input: .asciz "%d"
 	degrees: .word 0
-	output: .asciz "%d Fahrenheit is about %d Celcius\n"
+	output: .asciz "%d Fahrenheit is about %d Celsius\n"
