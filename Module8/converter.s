b44dee36c02f14b68b15c20793aecfee8d92c114
evbw <sp25bcd8b7@rpi-buster-armhf>
2025-03-14 18:21:34 +0000

    Added menu logic

---
+++ Module8/converter.s
@@ -0,0 +1,122 @@
+.global main
+
+.text
+main:
+	SUB sp, sp, #4
+	STR lr, [sp, #0]
+
+	LDR r0, =prompt
+	BL printf
+
+	LDR r0, =input
+	LDR r1, =num
+	BL scanf
+
+	LDR r0, =num
+	LDR r0, [r0]
+	MOV r1, #2
+	MOV r2, #3
+	MOV r3, #4
+
+	CMP r0, #1
+	BLE m2k
+	CMP r0, r1
+	BEQ kphour
+	CMP r0, r2
+	BEQ ctf
+	CMP r0, r3
+	BGE itf
+
+	m2k:
+		LDR r0, =m2kPrompt
+		BL printf
+		
+		LDR r0, =input
+		LDR r1, =num
+		BL scanf
+
+		LDR r0, =num
+		LDR r0, [r0]
+		BL miles2kilometer
+		MOV r1, r0
+		
+		LDR r0, =m2kOutput
+		BL printf
+		B EndProgram
+
+	kphour:
+		LDR r0, =kphPrompt1
+		BL printf
+
+		LDR r0, =input
+		LDR r1, =num
+		BL scanf
+
+		LDR r0, =num
+		LDR r4, [r0]
+
+		LDR r0, =kphPrompt2
+		BL printf
+
+		LDR r0, =input
+		LDR r1, =num
+		BL scanf
+
+		LDR r0, =num
+		LDR r1, [r0]
+		BL kph
+		MOV r1, r0
+		
+		LDR r0, =kphOutput
+		BL printf
+		
+		B EndProgram
+
+	ctf:
+
+		LDR r0, =m2kPrompt
+		BL printf
+		
+		LDR r0, =input
+		LDR r1, =num
+		BL scanf
+
+		LDR r0, =num
+		LDR r0, [r0]
+		BL miles2kilometer
+		MOV r1, r0
+		
+		B EndProgram
+
+	itf:
+
+		LDR r0, =m2kPrompt
+		BL printf
+		
+		LDR r0, =input
+		LDR r1, =num
+		BL scanf
+
+		LDR r0, =num
+		LDR r0, [r0]
+		BL miles2kilometer
+		MOV r1, r0
+		
+		B EndProgram
+
+	
+	EndProgram:
+
+	LDR lr, [sp, #0]
+	ADD sp, sp, #4
+	MOV pc, lr
+
+.data
+	prompt: .asciz "What would you like to do today?\nPlease enter 1 to convert miles to kilometers.\nPlease enter 2 to convert miles to kph.\nPlease enter 3 to convert from Celsius to Fahrenheit.\nPlease enter 4 to convert a number of inches to feet.\n"
+	m2kPrompt: .asciz "Please enter a number of miles to convert to kilometers:\n"
+	m2kOutput: .asciz "That is %d kilometers.\n"
+	kphPrompt1: .asciz "Please a number of miles:\n"
+	kphPrompt2: .asciz "Please enter a number of hours:\n"
+	kphOutput: .asciz "That is %d kph.\n"
+	input: .asciz "%d"
+	num: .word 0
