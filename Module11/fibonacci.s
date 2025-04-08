d5e193bf38cc83f088e98d8320b59e00665d9de7
evbw <sp25bcd8b7@rpi-buster-armhf>
2025-04-08 18:42:11 +0000

    Updated fibonacci logic

--- Module11/fibonacci.s
+++ Module11/fibonacci.s
@@ -19,17 +19,19 @@ main:
 		LDR r4, =num
 		LDR r4, [r4]
 
-		CMP r4, #0
-		BLE Invalid		//Handle an instance when the input is less than the required value
 		MOV r0, #0
+		MOV r1, #1
+
+		CMP r4, #0
+		BLT Invalid		//Handle an instance when the input is less than the required value
+		MOVEQ r1, #0
 		BEQ Answer
 
 		CMP r4, #1
-		MOVEQ r0, #1
 		BEQ Answer
 
 		MOV r1, #1
-		MOV r3, r4
+		MOV r6, r4
 		BL Fibo
 		MOV r1, r0
 
@@ -54,7 +56,7 @@ main:
 	MOV pc, lr
 
 .data
-	prompt: .asciz "Please enter a positive number (greater than 0) to find the value of the Fibonacci sequence at that poin:\n"
+	prompt: .asciz "Please enter a positive number (greater than 0) to find the value of the Fibonacci sequence at that point:\n"
 	invalidPrompt: .asciz "That was less than 1! Try again please from the beginning.\n"
 	output: .asciz "The result is %d.\n"
 	input: .asciz "%d"
@@ -62,26 +64,29 @@ main:
 
 .text
 Fibo:
-	SUB sp, sp, #8
+	SUB sp, sp, #12
 	STR lr, [sp]
 	STR r4, [sp, #4]
-
-	MOV r3, r1
+	STR r5, [sp, #8]
 
 	CMP r4, #1
-	BGT Else
-		B Return
-	Else:
-		ADD r0, r0, r3
-		ADD r4, r4, #-1
-		BL Fibo
-		B Return
+	BLE Return
+
+	ADD r4, r4, #-1
+	BL Fibo
+	MOV r5, r0
+
+	MOV r4, r6
+	ADD r4, r4, #-2
+	BL Fibo
+	ADD r0, r0, r5
 
 	Return:
 	
 	LDR lr, [sp]
 	LDR r4, [sp, #4]
-	ADD sp, sp, #8
+	LDR r5, [sp, #8]
+	ADD sp, sp, #12
 	MOV pc, lr
 
 .data
